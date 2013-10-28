0a3ffbfee810a2a492f74c301b0525283f60edb0
grizzlyadamson <grizzlyadamson@gmail.com>
2013-10-28 00:27:03 -0500

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Squatchapp::Application.config.secret_key_base = 'b17b656b939192349ce235d29d3adf67ab29e9c7d5cca9d28a82da469e900b55d799c4015213e4533453f7abe742c1097b18716d0b6e65383ef05b0cfade57cd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Squatchapp::Application.config.session_store :cookie_store, key: '_squatchapp_session'
