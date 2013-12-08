fb6cccbf243702c27cece7461c2e498d5376f3c7
grizzlyadamson <grizzlyadamson@gmail.com>
2013-12-08 01:05:25 -0600

    adding /contact page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Squatchapp::Application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
+  get 'static_pages/contact'
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
